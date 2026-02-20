93da50aa61406672a42310b1063f430a2033cb82
Szaszik-Adrian <szaszik.adrian@gmail.com>
2026-02-20 10:48:50 +0100

    consol

---
+++ RealEstate/RealEstate/Ad.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace RealEstate
+{
+    public class Ad
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = "";
+        public int Rooms { get; set; }
+        public int Area { get; set; }
+        public int Floors { get; set; }
+        public Category Category { get; set; } = new Category(0, "");
+        public Seller Seller { get; set; } = new Seller(0, "", "");
+        public bool FreeOfCharge { get; set; }
+        public string ImageUrl { get; set; } = "";
+        public string LatLong { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+
+        private static bool ParseBool(string s)
+        {
+            s = s.Trim().ToLowerInvariant();
+            return s == "true" || s == "1" || s == "igen" || s == "yes";
+        }
+
+        public double DistanceTo(double lat, double lon)
+        {
+            string[] t = LatLong.Split(',');
+            double myLat = double.Parse(t[0], System.Globalization.CultureInfo.InvariantCulture);
+            double myLon = double.Parse(t[1], System.Globalization.CultureInfo.InvariantCulture);
+
+            double a = myLat - lat;
+            double b = myLon - lon;
+
+            return Math.Sqrt(a * a + b * b);
+        }
+
+        // 3-5. feladat
+        public static List<Ad> LoadFromCsv(string fileName)
+        {
+            var ads = new List<Ad>();
+            var lines = File.ReadAllLines(fileName);
+
+            if (lines.Length < 2)
+                return ads;
+
+            char sep = lines[0].Contains(";") ? ';' : ',';
+            var header = lines[0].Split(sep);
+
+            var col = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            for (int i = 0; i < header.Length; i++)
+                col[header[i].Trim()] = i;
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i])) continue;
+
+                var p = lines[i].Split(sep);
+
+                int id = int.Parse(p[col["id"]]);
+                string desc = p[col["description"]];
+                int rooms = int.Parse(p[col["rooms"]]);
+                int area = int.Parse(p[col["area"]]);
+                int floors = int.Parse(p[col["floors"]]);
+
+                ads.Add(new Ad
+                {
+                    Id = id,
+                    Description = desc,
+                    Rooms = rooms,
+                    Area = area,
+                    Floors = floors,
+
+                    FreeOfCharge = ParseBool(p[col["freeOfCharge"]]),
+                    ImageUrl = p[col["imageUrl"]],
+                    LatLong = p[col["latlong"]],
+                    CreatedAt = DateTime.Parse(p[col["createAt"]], CultureInfo.InvariantCulture),
+
+                    Category = new Category(
+                    int.Parse(p[col["categoryId"]]),
+                    p[col["categoryName"]]
+                    ),
+
+                    Seller = new Seller(
+                    int.Parse(p[col["sellerId"]]),
+                    p[col["sellerName"]],
+                    p[col["sellerPhone"]]
+                        )
+                });
+            }
+
+            return ads;
+        }
+    }
+}
---
+++ RealEstate/RealEstate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealEstate;
+using System.Globalization;
+
+namespace RealEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ingatlanok";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string csvFile = "realestates.csv"; 
+
+            List<Ad> ads = Ad.LoadFromCsv(csvFile);
+
+            // 6. feladat: földszinti (floors == 0) átlag alapterület
+            var ground = ads.Where(a => a.Floors == 0).ToList();
+
+            double avg = ground.Average(a => a.Area);
+
+            // magyar mintában gyakran vesszővel jelenik meg, de biztosra:
+            Console.WriteLine("1. Földszinti ingatlanok átlagos alapterülete: " +
+                              avg.ToString("0.00", CultureInfo.InvariantCulture) + " m2");
+
+            double ovodaLat = 47.4164220114023;
+            double ovodaLon = 19.066342425796986;
+
+            var nearestFree = ads
+                .Where(a => a.FreeOfCharge)
+                .OrderBy(a => a.DistanceTo(ovodaLat, ovodaLon))
+                .First();
+
+            Console.WriteLine("2. Mesévár óvodához légvonalban legközelebbi tehermentes ingatlan adatai:");
+            Console.WriteLine("Eladó neve    : " + nearestFree.Seller.Name);
+            Console.WriteLine("Eladó telefonja: " + nearestFree.Seller.Phone);
+            Console.WriteLine("Alapterület   : " + nearestFree.Area);
+            Console.WriteLine("Szobák száma  : " + nearestFree.Rooms);
+
+            Console.ReadLine();
+        }
+    }
+}
